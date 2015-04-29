d83cf63ee5be917838c0ad6df20b11dfd81b152d
LyzhinIvan <lyzhin-ivan@yandex.ru>
2015-04-29 21:39:17 +0500

    Some modification

--- Naumen/Program.cs
+++ Naumen/Program.cs
@@ -56,6 +56,7 @@ namespace Naumen
                     Console.WriteLine(variant);
                 }
             }
+            
         }
 
         static List<string> GetRandomClassNames(int quantity)
